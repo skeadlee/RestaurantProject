de8915e786fad7332c09ee6196b3af35a09ef26b
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-12 14:32:55 +0100

    create a basic Customer class

--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -1,4 +1,49 @@
 package models;
 
 public class Customer {
+
+    private int id;
+    private String name;
+    private int wallet;
+    private int loyaltyCard;
+
+    public Customer(){}
+
+    public Customer(String name, int wallet) {
+        this.name = name;
+        this.wallet = wallet;
+        this.loyaltyCard = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(int wallet) {
+        this.wallet = wallet;
+    }
+
+    public int getLoyaltyCard() {
+        return loyaltyCard;
+    }
+
+    public void setLoyaltyCard(int loyaltyCard) {
+        this.loyaltyCard = loyaltyCard;
+    }
 }
