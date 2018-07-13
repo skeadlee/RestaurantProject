b2d169a5b27206e21d71e71dad24b7ab11dc109e
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-13 20:13:06 +0100

    add basic booking function

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -1,17 +1,26 @@
+import models.Booking;
+import models.Customer;
 import models.Restaurant;
 import models.Seating;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import static org.junit.Assert.assertEquals;
 
 public class RestaurantTest {
 
     Restaurant restaurant;
+    Booking booking1;
+    Calendar calendar1;
+    Customer customer1;
 
     @Before
     public void setUp() throws Exception {
         restaurant = new Restaurant("CodeClanChristmasCookHouse");
+        booking1 = new Booking((GregorianCalendar)calendar1, 4, customer1, Seating.TABLEFOUR);
     }
 
     @Test
@@ -24,5 +33,11 @@ public class RestaurantTest {
         assertEquals(true, restaurant.getTables().contains(Seating.TABLETWO));
         assertEquals(true, restaurant.getTables().contains(Seating.TABLEFOUR));
         assertEquals(true, restaurant.getTables().contains(Seating.TABLESIX));
+    }
+
+    @Test
+    public void canMakeBooking(){
+        restaurant.makeBooking(booking1);
+        assertEquals(1, restaurant.getBooking().size());
     }
 }
