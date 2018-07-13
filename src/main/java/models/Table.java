24693b71c82444e4f9016aa84f15b7333f41e14d
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-13 10:08:13 +0100

    Add table enum with values

--- src/main/java/models/Table.java
+++ src/main/java/models/Table.java
@@ -1,4 +1,35 @@
 package models;
 
-public class Table {
+import java.util.ArrayList;
+
+public enum Table {
+
+    TABLETWO(2, "Table for two"),
+    TABLEFOUR(4, "Table for four"),
+    TABLESIX(6, "Table for six");
+
+    private int id;
+    private int chairs;
+    private String name;
+    private ArrayList<Customer> diners;
+
+    Table(int chairs, String name) {
+
+        this.chairs = chairs;
+        this.name = name;
+        this.diners = new ArrayList<Customer>();
+    }
+
+    public int getChairs() {
+        return chairs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getDiners() {
+        return diners;
+    }
+    
 }
