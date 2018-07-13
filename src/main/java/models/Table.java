de5b375ae2df5002a06fe85ff80c2e72333a0461
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-13 14:05:14 +0100

    add timeslots to tables

--- src/main/java/models/Table.java
+++ src/main/java/models/Table.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public enum Table {
 
@@ -11,12 +12,17 @@ public enum Table {
     private int chairs;
     private String name;
     private ArrayList<Customer> diners;
+    private ArrayList<TimeSlot> timeSlots;
+
 
     Table(int chairs, String name) {
 
         this.chairs = chairs;
         this.name = name;
         this.diners = new ArrayList<Customer>();
+        this.timeSlots = new ArrayList<>(Arrays.asList(TimeSlot.SEVEN, TimeSlot.SEVENTHIRTY, TimeSlot.EIGHT, TimeSlot.EIGHTTHIRTY,
+        TimeSlot.NINE, TimeSlot.NINETHIRTY, TimeSlot.TEN, TimeSlot.TENTHIRTY, TimeSlot.ELEVEN));
+
     }
 
     public int getChairs() {
