bf64ac6718f25ce87725ee1907c5fe044c40afab
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-14 11:19:24 +0100

    changed customers in booking to arraylist

--- src/main/java/models/Booking.java
+++ src/main/java/models/Booking.java
@@ -3,14 +3,16 @@ package models;
 import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class Booking {
 
     private int id;
     private GregorianCalendar dateTime;
     private int capacity;
-    private Customer customer;
+    private List<Customer> customers;
     private Seating seating;
     private int pricePerHead;
     private double discountApplied;
@@ -23,7 +25,7 @@ public class Booking {
     public Booking(GregorianCalendar dateTime, int capacity, Customer customer, Seating seating) {
         this.dateTime = dateTime;
         this.capacity = capacity;
-        this.customer = customer;
+        this.customers = new ArrayList<>();
         this.seating = seating;
         this.pricePerHead = 50;
         this.discountApplied = 35;
@@ -64,13 +66,16 @@ public class Booking {
     @JoinTable(name = "customer_booking",
             joinColumns = {@JoinColumn(name = "booking_id", nullable = false, updatable = false)},
             inverseJoinColumns = {@JoinColumn(name = "customer_id", nullable = false, updatable = false)})
-    public Customer getCustomer() {
-        return customer;
+
+    public List<Customer> getCustomers() {
+        return customers;
     }
 
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
     }
+
+    public void addCustomers(Customer customer){ this.customers.add(customer);}
 
 
     @Enumerated(value = EnumType.STRING)
