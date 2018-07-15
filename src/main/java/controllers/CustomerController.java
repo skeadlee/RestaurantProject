add43d893b2b172f84071bcc7cbc6045b2456ebe
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-15 19:31:38 +0100

    Add customer show and edit page

--- src/main/java/controllers/CustomerController.java
+++ src/main/java/controllers/CustomerController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class CustomerController {
 
@@ -24,5 +25,44 @@ public class CustomerController {
             model.put("customers", customers);
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
-        }
+
+        get("customers/:id/edit", (req, res) -> {
+            String stringInt = req.params(":id");
+            Integer intId = Integer.parseInt(stringInt);
+            Customer customer = DBHelper.find(intId, Customer.class);
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("customer", customer);
+            model.put("template", "templates/customers/edit.vtl");
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/customers/:id", (req, res) -> {
+            String stringInt = req.params(":id");
+            Integer idInt = Integer.parseInt(stringInt);
+            Customer customer = DBHelper.find(idInt, Customer.class);
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("customer", customer);
+            model.put("template", "templates/customers/show.vtl");
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/customers/:id", (req, res) -> {
+            String stringInt = req.params(":id");
+            Integer idInt = Integer.parseInt(stringInt);
+            Customer customer = DBHelper.find(idInt, Customer.class);
+            String name = req.queryParams("name");
+            int loyaltyCard = Integer.parseInt(req.queryParams("loyaltyCard"));
+
+            customer.setName(name);
+            customer.setLoyaltyCard(loyaltyCard);
+            DBHelper.save(customer);
+            res.redirect("/customers");
+            return null;
+        }, new VelocityTemplateEngine());
+
+    }
 }
